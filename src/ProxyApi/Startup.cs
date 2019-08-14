4b42420b0831c8715619b5f21ea6384df41349f0
AbolfazlRajabpour <Abolfazl.rajabpour@gmail.com>
2019-08-14 19:11:50 +0430

    disable https redirection

--- src/ProxyApi/Startup.cs
+++ src/ProxyApi/Startup.cs
@@ -41,7 +41,7 @@ namespace ProxyApi
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }

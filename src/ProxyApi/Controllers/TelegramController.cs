41c7128e8155f324850958753f7d60ea2be62c03
AbolfazlRajabpour <Abolfazl.rajabpour@gmail.com>
2019-08-14 19:42:58 +0430

    phot message

--- src/ProxyApi/Controllers/TelegramController.cs
+++ src/ProxyApi/Controllers/TelegramController.cs
@@ -14,18 +14,22 @@ namespace ProxyApi.Controllers
     public class TelegramController : ControllerBase
     {
         [HttpPost("send")]
-        public async Task NewMessage([FromBody] NewTelegramMessage newTelegramMessage)
+        public async Task<string> NewMessage(NewTelegramMessage newTelegramMessage)
         {
             var botClient = new Telegram.Bot.TelegramBotClient(newTelegramMessage.BotToken);
             var me = await botClient.GetMeAsync();
 
             if (newTelegramMessage.File != null)
             {
-                //var resultPhotoMessage = await botClient.SendPhotoAsync(newTelegramMessage.ChatId,new InputMedia(), newTelegramMessage.Text);
+                var memoryStream = new MemoryStream();
+                await newTelegramMessage.File.CopyToAsync(memoryStream);
+                var resultPhotoMessage = await botClient.SendPhotoAsync(newTelegramMessage.ChatId, memoryStream, newTelegramMessage.Text);
+                return resultPhotoMessage.MessageId.ToString();
             }
             else
             {
                 var resultTextMessage = await botClient.SendTextMessageAsync(newTelegramMessage.ChatId, newTelegramMessage.Text);
+                return resultTextMessage.MessageId.ToString();
             }
         }
     }
